a8233fe7329714fff5ba47081812382a85b4e2b6
BothThumbUp <mtyskows@gmail.com>
2018-03-24 14:04:33 -0700

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+    render html: "I swear if i have to do this again"
+  end
+
+  def goodbye
+    render html: "Bye Bye Bye - Nsync"
+  end
+
 end
